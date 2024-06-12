f3c76b45de249e5bca8b50e1aeadcfa329139039
srinivasettagoni <srinivasmudhiraj25196@gmail.com>
2024-06-12 17:51:07 +0530

    Javascript

---
+++ Javascript/Day 11/Task/Datatype-Corecion.js
@@ -0,0 +1,39 @@
+// Data Types
+// ==============
+
+// String
+// var favoriteFood = 'Biryani'
+// console.log(favoriteFood)
+
+// // Number
+// var myAge = 25;
+// console.log(myAge);
+
+// // Boolean
+// var isStudent = true;
+// console.log(isStudent);
+
+// // Undefined
+// var myPet;
+// console.log(myPet);
+
+// // null
+// var carBrand = null;
+// console.log(carBrand)
+
+// Coercion
+// ============
+// console.log("1"+"4"+"1")     //141
+// console.log("A"-"B")        //Nan
+// console.log("A"-"B"+"1")        //Nan1
+// console.log("A"-"B"+1)            //  Nan
+// console.log("1"-"1")            //0
+// console.log(+"1"+"1"+"1")           //111
+// console.log(1 + + "1" + "1")        //21
+// console.log(1 - "1" + "1")              //01
+// console.log(+ " ")                       //0
+// console.log(- " ")                      //-0
+
+
+
+
---
+++ Javascript/Day 11/dataType.js
@@ -0,0 +1,36 @@
+var data = "Hello World"
+var message = "Welcome Friend"
+// console.log(data)
+// console.log(data +" "+ message)
+
+
+
+
+// console.log(data.length)
+// console.log(data.charAt(1))
+// console.log(data.toUpperCase())
+// console.log(data.toLowerCase())
+
+
+// var number = 10;
+// var numInt = 5;
+// var numFloat = 2.325487; 
+// var num = 2.548795
+
+//  console.log(num.toFixed(3))
+
+// console.log(1 + + "9" + "2" );
+
+// var square = Math.sqrt(50);
+// var absolute = Math.abs(-6);
+// var min = Math.max(30,25,85,1)
+
+// console.log(min)
+
+// var isStudent = true;
+// var num = Boolean(true);
+
+// console.log(num)
+
+// var data1 = null;
+// console.log(data1)
